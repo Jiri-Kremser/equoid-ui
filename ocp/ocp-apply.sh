5648e63b9d0fc21323a126c409b46bfd642164fa
Jirka Kremser <jkremser@redhat.com>
2018-02-27 18:35:05 +0100

    simplifying the OC resources... no need for special sevice account if all the images can be run as non-root

--- ocp/ocp-apply.sh
+++ ocp/ocp-apply.sh
@@ -5,29 +5,27 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 oc new-project equoid
 
 # workaround for postgresql docker image that requires root
-oc login -u system:admin
-oc adm policy add-scc-to-user anyuid system:serviceaccount:equoid:equoid
-oc login -u developer
+#oc login -u system:admin
+#oc adm policy add-scc-to-user anyuid system:serviceaccount:equoid:equoid
+#oc login -u developer
 
 
 # Use this script to run oc commands to create resources in the selected namespace. Files are ordered
 # in proper order. 'oc process' processes the template as resources which is again piped to
 # 'oc apply' to create those resources in OpenShift namespace
-oc process -f ./ocp/registry/scc-config.yml | oc apply -f -
+#oc process -f $DIR/registry/scc-config.yml | oc apply -f -
 
 
 # Prometheus and Grafana
-#oc process -f ./ocp/monitoring/metrics.yml | oc apply -f -
+oc process -f $DIR/monitoring/metrics.yml | oc apply -f -
 
 
 # Keycloak SSO
 # example realm json: https://github.com/kameshsampath/keycloak-demo-server/blob/master/src/main/resources/springboot-realm.json
 oc secrets new kc-realm jhipster-realm.json=$DIR/../src/main/docker/realm-config/jhipster-realm.json
 oc secrets new kc-users jhipster-users-0.json=$DIR/../src/main/docker/realm-config/jhipster-users-0.json
-oc process -f ./ocp/keycloak/keycloak.yml | oc apply -f -
+oc process -f $DIR/keycloak/keycloak.yml | oc apply -f -
 
-
-#oc process -f ./ocp/equoid/equoid-postgresql.yml | oc apply -f -
 
 # Finally, deploy the Equoid app
-oc process -f ./ocp/equoid/equoid-deployment.yml | oc apply -f -
+oc process -f $DIR/equoid/equoid-deployment.yml | oc apply -f -
