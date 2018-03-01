04189cbc07a67934ad4c88331a5f08ec5c73e7a2
Jirka Kremser <jkremser@redhat.com>
2018-03-01 18:01:48 +0100

    workaround to https://github.com/jhipster/generator-jhipster/issues/7188

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -123,7 +123,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             minimize: true,
             debug: false
         }),
-        new WorkboxPlugin({
+        new WorkboxPlugin.GenerateSW({
           clientsClaim: true,
           skipWaiting: true,
         })
