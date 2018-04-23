0dbd283ff1f18c0dc0b049eb6efd1a08811e3d44
Jirka Kremser <jkremser@redhat.com>
2018-04-23 15:57:11 +0200

    hot-fix: publisher url

--- ocp/ocp-apply.sh
+++ ocp/ocp-apply.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+
 DIR="${DIR:-$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )}"
 BASE_URL="${BASE_URL:-$DIR}"
 KC_REALM_PATH="${KC_REALM_PATH:-"$DIR/../src/main/docker/realm-config/"}"
@@ -26,6 +28,8 @@ else
   echo "Equoid app will be using this keycloak instance: http://$KC_ROUTE"
 fi
 
+echo "KEYCLOAK_URL=$KC_ROUTE"
+echo "PUBLISHER_URL=$PUBLISHER_URL"
 oc process -f https://raw.githubusercontent.com/Jiri-Kremser/equoid-ui/master/ocp/equoid/equoid-deployment.yml \
   -p KEYCLOAK_URL=$KC_ROUTE \
   -p PUBLISHER_URL=$PUBLISHER_URL \
