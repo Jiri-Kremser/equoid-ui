e4e8ae095810bb9bc2233d27eb5a6afa464ef79d
Jirka Kremser <jkremser@redhat.com>
2018-04-23 16:09:44 +0200

    hotfix

--- ocp/ocp-apply.sh
+++ ocp/ocp-apply.sh
@@ -16,9 +16,6 @@ oc secrets new kc-realm jhipster-realm.json=$KC_REALM_PATH/jhipster-realm.json
 oc secrets new kc-users jhipster-users-0.json=$KC_REALM_PATH/jhipster-users-0.json
 oc process -f $BASE_URL/keycloak/keycloak.yml | oc apply -f -
 
-# Finally, deploy the Equoid app
-oc process -f $BASE_URL/equoid/equoid-deployment.yml | oc apply -f -
-
 KC_ROUTE=`oc get routes -l app=equoid-keycloak --no-headers | awk '{print $2}'`
 PUBLISHER_URL="equoid-data-publisher:8080"
 if [[ $KC_ROUTE = *"127.0.0."* ]]; then
@@ -33,4 +30,4 @@ echo "PUBLISHER_URL=$PUBLISHER_URL"
 oc process -f https://raw.githubusercontent.com/Jiri-Kremser/equoid-ui/master/ocp/equoid/equoid-deployment.yml \
   -p KEYCLOAK_URL=$KC_ROUTE \
   -p PUBLISHER_URL=$PUBLISHER_URL \
-  | oc create -f -
+  | oc apply -f -
